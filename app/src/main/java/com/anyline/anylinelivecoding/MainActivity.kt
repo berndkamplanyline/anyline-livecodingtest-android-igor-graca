72dcaef714c44a907f3db72d108fd73453896f74
Bernd Kampl <bernd.kampl@anyline.com>
2022-05-18 16:46:23 +0200

    Implement Camera Permissions, LeakCanary

--- app/src/main/java/com/anyline/anylinelivecoding/MainActivity.kt
+++ app/src/main/java/com/anyline/anylinelivecoding/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.anyline.anylinelivecoding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.anyline.anylinelivecoding.databinding.ActivityMainBinding
+import timber.log.Timber
 
 private lateinit var binding: ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : CameraPermissionActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.requestCameraPermissionButton.setOnClickListener {
+            executeIfCameraPermissionGranted {
+                Timber.d("--- Camera permission has been granted!")
+
+                /* TODO implement whatever we want to do with granted Camera Permission*/
+
+            }
+        }
     }
 }
