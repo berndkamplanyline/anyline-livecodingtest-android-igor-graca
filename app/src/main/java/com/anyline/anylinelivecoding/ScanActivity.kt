54097b3cb33ed62a5f7bf0e81f425bc796a0e85b
Bernd Kampl <bernd.kampl@anyline.com>
2022-05-24 10:02:14 +0200

    Add task description

--- app/src/main/java/com/anyline/anylinelivecoding/ScanActivity.kt
+++ app/src/main/java/com/anyline/anylinelivecoding/ScanActivity.kt
@@ -15,7 +15,22 @@ class ScanActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        // TODO: continue here with setting up the examples stuff.
+        /**
+         * TODO: Implement Barcode Scanning using the Anyline SDK.
+         * Expected Result: Scan a Barcode and return the scanned value to the User.
+         * Hints:
+         * - Use the documentation: https://documentation.anyline.com/toc/platforms/android/getting_started.html
+         * - Use the Anyline Examples App: https://github.com/Anyline/anyline-ocr-examples-android/blob/master/AnylineSDK-Examples/AnylineSDK-Examples-Source/app/src/main/java/io/anyline/examples/barcode/ScanBarcodeActivity.kt
+         * - Ignore anything License-related (this part is solved!)
+         * - Ignore requesting Permissions (already implemented in CameraPermissionActivity.kt)
+         * - For easier Logging, Timber can be used: Timber.d("This is Log Output")
+         * - To access views, ViewBinding can be used: binding.[Widget]
+         * - View Configuration is available in assets/barcode_view_config.json
+         * - Getting a result: Use (both works) ScanViewPlugin or ScanPlugin and attach (both works
+         *   again) a ScannedBarcodesListener or a ScanResultListener. Extract the required value
+         *   from the returned result and display it to the user.
+         * - Wrap initializing the Anyline SDK into try/catch!
+         */
 
         AnylineSDK.init(getString(R.string.anyline_license_key), this)
     }
